70900c9122c7b8d64e462f93a518042ba1158925
benbueltmann <benbueltmann@gmail.com>
2016-01-25 13:14:25 -0600

    Initial Commit

---
+++ Assessment3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Assessment3
+//
+//  Created by Ben Bueltmann on 1/25/16.
+//  Copyright © 2016 Mobile Makers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
